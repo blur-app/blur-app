56c3dfc6469eee5a60da9a66de20b0c3e322df30
Devin Matte <devin@devinmatte.com>
2019-02-09 21:41:42 -0500

    Icons for UI

---
+++ components/Reactions.js
@@ -0,0 +1,12 @@
+import {ButtonGroup} from "react-native-elements";
+import React from "react";
+
+export default ({id}) => {
+
+  //TODO: Retrieve the Reactions list from API for Post ID
+
+  return (
+    <ButtonGroup
+    />
+  );
+};
